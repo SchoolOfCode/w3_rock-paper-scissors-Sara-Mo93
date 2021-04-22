16e8fb86ce5c8d384cd8a2d93219d949d43e5d7a
Sara Mohamoud <sara_mohamoud@outlook.com>
2021-04-22 21:33:32 +0100

    first draft

---
+++ main.js
@@ -0,0 +1,98 @@
+const playerScore = document.getElementById("userScore");
+const computerScore = document.getElementById("cuScore");
+const rockemoji = document.getElementById("rockbtn");
+const paperemoji = document.getElementById("pbtn");
+const scissoremoji = document.getElementById("scsbtn");
+const userInput = document.getElementById("playername"); //would be nice to display name as player 
+
+let pScore = 0;
+let cScore = 0;
+let round = 0;
+let userChoice = rockemoji || paperemoji || scissoremoji;
+let computerChoice;
+rockemoji.addEventListener("click", play);
+paperemoji.addEventListener("click", play);
+scissoremoji.addEventListener("click", play);
+// removed document.write("Hey " + name); let name = window.prompt("What is your username"); need to use html input instead and then use the dom to retrieve input//
+
+function getComputerChoice() {
+  computerChoice = Math.random();
+  if (computerChoice < 0.34) {
+    computerChoice = "rock";
+    return "rock";
+  } else if (computerChoice <= 0.67) {
+    computerChoice = "paper";
+    return "paper";
+  } else {
+    computerChoice = "scissors";
+    return "scissors";
+  }
+}
+
+function play(event) {
+  getComputerChoice();
+  getWinner(userChoice, computerChoice);
+  return;
+}
+
+// need to find a shorter way around this- refactor .
+function getWinner(userChoice, computerChoice) {
+  document.getElementById("GameRound").innerHTML = "Round " + round;
+  round++;
+  if (userChoice === rockemoji && computerChoice === "rock") {
+    document.getElementById("userScore").innerHTML = "Player Score " + pScore;
+    document.getElementById("cuScore").innerHTML = "Computer Score " + cScore;
+    console.log(
+      "Player chooses rock! Computer chooses rock! It's a draw in this round!"
+    );
+  } else if (userChoice === paperemoji && computerChoice === "paper") {
+    document.getElementById("userScore").innerHTML = "Player Score " + pScore;
+    document.getElementById("cuScore").innerHTML = "Computer Score " + cScore;
+    console.log(
+      "Player chooses paper! Computer chooses paper! It's a draw in this round!"
+    );
+  } else if (userChoice === scissoremoji && computerChoice === "scissors") {
+    document.getElementById("userScore").innerHTML = "Player Score " + pScore;
+    document.getElementById("cuScore").innerHTML = "Computer Score " + cScore;
+    console.log(
+      "Player chooses scissors! Computer chooses scissors! It's a draw in this round!"
+    );
+  } else if (userChoice === rockemoji && computerChoice === "paper") {
+    document.getElementById("cuScore").innerHTML = "Computer Score " + cScore++;
+    document.getElementById("userScore").innerHTML = "Player Score " + pScore;
+    console.log(
+      "Player chooses rock! Computer chooses paper! Computer wins this round!"
+    );
+  } else if (userChoice === rockemoji && computerChoice === "scissors") {
+    document.getElementById("cuScore").innerHTML = "Computer Score " + cScore;
+    document.getElementById("userScore").innerHTML = "Player Score " + pScore++;
+    console.log(
+      "Player chooses rock! Computer chooses scissors! Player wins this round!"
+    );
+  } else if (userChoice === paperemoji && computerChoice === "scissors") {
+    document.getElementById("cuScore").innerHTML = "Computer Score " + cScore++;
+    document.getElementById("userScore").innerHTML = "Player Score " + pScore;
+    console.log(
+      "Player chooses paper! Computer chooses scissors! Computer wins this round!"
+    );
+  } else if (userChoice === paperemoji && computerChoice === "rock") {
+    document.getElementById("cuScore").innerHTML = "Computer Score " + cScore;
+    document.getElementById("userScore").innerHTML = "Player Score " + pScore++;
+    console.log(
+      "Player chooses paper! Computer chooses rock! Player wins this round!"
+    );
+  } else if (userChoice === scissoremoji && computerChoice === "rock") {
+    document.getElementById("cuScore").innerHTML = "Computer Score " + cScore++;
+    document.getElementById("userScore").innerHTML = "Player Score " + pScore;
+    console.log(
+      "Player chooses scissors! Computer chooses rock! Computer wins this round!"
+    );
+  } else if (userChoice === scissoremoji && computerChoice === "paper") {
+    document.getElementById("cuScore").innerHTML = "Computer Score " + cScore;
+    document.getElementById("userScore").innerHTML = "Player Score " + pScore++;
+    console.log(
+      "Player chooses scissors! Computer chooses paper! Player wins this round! "
+    );
+  }
+  return;
+}
